a438b162ebcadab8f4da4f783cb82be4aff71a1b
wogml6510 <wogml6510@naver.com>
2023-05-14 15:07:55 +0900

    에산수립 목록 + 등록화면 구현완료

---
+++ src/main/java/com/ducks/demys/boot/controller/BudgetController.java
@@ -0,0 +1,23 @@
+package com.ducks.demys.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BudgetController {
+	
+	@RequestMapping("budget/main")
+	public void showBudgetMain() {
+		
+	}
+	
+	@RequestMapping("budget/list")
+	public void showBudgetList() {
+		
+	}
+	
+	@RequestMapping("budget/regist")
+	public void showBudgetRegist() {
+		
+	}
+}
